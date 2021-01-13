c86b64d385269fac82ce875ea464f40817d6efc9
matheusF99 <mc79846@gmail.com>
2021-01-13 17:39:17 -0300

    adicionando as tipagens

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import api from '../services/api'
 
+
+interface userProps{
+  name: string,
+  email: string
+}
+
+
 function App() {
   
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState<userProps[]>([])
 
   useEffect(()=>{
-      api.get('/users').then(response => {
+      api.get<userProps[]>('/users').then(response => {
         setUsers(response.data)
       })
   },[])
 
   return (
     <div className="App">
-      
+        {
+          users.map(user => <p>{user}</p>)
+        }
     </div>
   );
 }
